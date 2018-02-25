b05a6fb96e8ade45ced0462bfa1d8a653abbe128
quique1234 <quique00@hotmail.es>
2018-02-25 13:10:08 -0600

    TERMINADO

---
+++ RedNeoronalesS14120070.py
@@ -0,0 +1,7 @@
+#LUIS ENRIQUE GUZMAN NIÑO S14120070
+entradasA_B = ((0, 0), (0 ,1), (1, 0), (1, 1))
+pesos = (-10, 20, 20)
+print('g({0} + ({1} * x1) + ({2} * x2)'.format(pesos[0], pesos[1], pesos[2]))
+for i in entradasA_B:
+    res = pesos[0] + (20 * i[0] + (20 * i[1]))
+    print('g({0} + ({1} * {2}) + ({3} * {4})) = {5}'.format(pesos[0], pesos[1], i[0], pesos[2], i[1], res))
